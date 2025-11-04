b161006fc739ed85f6b6f1e735e486cdf140cdd9
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-11-03 21:53:46 -0600

    init commit

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+function NavBar() {
+
+    return (
+        <nav>
+            <ul>
+                <li>About Me</li>
+                <li>Portfolio</li>
+                <li>Social</li>
+
+                {/* 
+                li><link to=''>About Me</link></li>
+                <li><link to=''>Portfolio</link></li>
+                <li><link to=''>Social</link></li> */}
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
